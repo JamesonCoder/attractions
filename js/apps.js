32c6c7765e941b925c4ed447f4a06b830e1b21e2
JamesonCoder <jamesonhuang@jamesonsdogmbp.gateway.pace.com>
2016-09-22 23:30:46 -0700

    phase 1

---
+++ js/apps.js
@@ -0,0 +1,19 @@
+
+
+
+ /*$(".heart").hide().show('slow').hide().show('slow').effect('bounce', {timer: 2}, 200);
+
+ $(".where").hide().show('slow');
+
+
+ $(".diverse").hide().show('slow').fade(); */
+
+
+
+$(document).ready(function() {
+	$('.where').hide('slow').show('slow');
+});
+
+/*$(document).ready(function() {
+	$('.dub').hide('fast').show('slow');
+}); */
